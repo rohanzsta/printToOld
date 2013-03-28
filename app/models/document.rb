cf99485bf9f53c305b68a5070e4b301b7315e8cb
iansampaio <iansampaio@gmail.com>
2013-03-28 04:54:15 +0100

    Added Documents to the header and the hability to open PDF

--- app/models/document.rb
+++ app/models/document.rb
@@ -4,7 +4,7 @@ class Document < ActiveRecord::Base
   validates :title, presence: true
   validates :user_id, presence: true
   validates_attachment :image, presence: true,
-  															content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
+  															content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif', 'application/pdf', 'application/x-pdf'] },
   															size: { less_than: 5.megabytes }
 
   belongs_to :user
